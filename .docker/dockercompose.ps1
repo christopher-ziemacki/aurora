1ad84c3463ec33ac6b0ac29590e4d40eee8b7c14
Christopher Ziemacki <52426440+christopher-ziemacki@users.noreply.github.com>
2021-02-27 14:15:23 +0200

    Modifications to the dockercompose.ps1 file

--- .docker/dockercompose.ps1
+++ .docker/dockercompose.ps1
@@ -1,1 +1,19 @@
-docker-compose -p aurora -f .\.docker\dockercompose.debug.yml up --build
+param([string]$configuration="debug", [switch]$build)
+
+$configuration = $configuration.ToLower();
+
+$dockerComposeParameters = ""
+
+if($build) {
+    $dockerComposeParameters += "--build";
+}
+
+if($configuration -eq "debug") {
+    docker-compose -p aurora -f .docker\dockercompose.debug.yml up $dockerComposeParameters
+}
+elseif ($configuration -eq "watch") {
+    docker-compose -p aurora -f .docker\dockercompose.watch.yml up $dockerComposeParameters
+}
+else { 
+    Write-Host "Configuration '$configuration' is not known"
+}
---
+++ dockercompose.ps1
@@ -0,0 +1,1 @@
+./.docker/dockercompose.ps1
